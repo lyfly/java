ada87da76a31f2eb79f96e3ed860f780bb40f61f
lyfly <383797841@qq.com>
2017-11-23 16:02:54 +0800

    删除DAO

--- DAO学习/src/StudentDAO.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public interface StudentDAO {
-    public abstract void addStudent(Student s);
-    public abstract void deleteStudent(Student s);
-    public abstract List<Student> getAllStudents();
-}
